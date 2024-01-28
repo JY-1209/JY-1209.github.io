3c44bab789dd8c7fdc56a9b91bdf921bf9590e67
Justin Yang <justyn17@cs.washington.edu>
2024-01-28 15:49:08 -0800

    added backgrounds

---
+++ index.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  $(".oh")
+    .mouseenter(function () {
+      $(".card").stop().animate(
+        {
+          top: "-90px",
+        },
+        "slow"
+      );
+    })
+    .mouseleave(function () {
+      $(".card").stop().animate(
+        {
+          top: 0,
+        },
+        "slow"
+      );
+    });
+  $(".oh").click(function () {
+    $(".first").css("display", "none")
+    $(".second").css("display", "block")
+  });
+});
